bc1d7c7f2a6ede511b194e8e6e90fa7c249a60a8
RicardoLopes <armindoricardo656@gmail.com>
2026-05-25 16:05:28 -0100

    Added better exception handling and data validation
    Made updates to all classes

--- modules/entities.py
+++ modules/entities.py
@@ -1,5 +1,6 @@
 from db_config import connection, cursor
 from datetime import date, time, datetime, timedelta
+import re
 
 class Rooms:
     def __init__(self, id, bed_qty: int, category, type, price_per_night: float = 0.0, occupied: int = 0, description = None):
@@ -11,10 +12,60 @@ class Rooms:
         self.description = description
         self.price_per_night = price_per_night
         self.add_room()
-    
+        
     @property
     def id(self):
         return self.__id
+
+    @property
+    def category(self):
+        return self.__category
+
+    @category.setter
+    def category(self, value):
+        try:
+            if value not in ["Regular", "With View"]:
+                raise ValueError(f"Room Category can only be 'Regular' or 'With View', got {value}"")
+            else:
+                self.__category = value
+            con = connection
+            curs = cursor
+    
+            query = "UPDATE rooms SET category = %s WHERE id = %d"
+            params = (self.category(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
+
+    @property
+    def type(self):
+        return self.__category
+
+    @type.setter
+    def type(self, value):
+        try:
+            if value not in ["Suite", "Single"]:
+                raise ValueError(f"Room Type can only be 'Suite' or 'Single', got {value}"")
+            else:
+                self.__type = value
+            con = connection
+            curs = cursor
+    
+            query = "UPDATE rooms SET type = %s WHERE id = %d"
+            params = (self.type(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
 
     @property
     def occupied(self):
@@ -22,38 +73,65 @@ class Rooms:
     
     @occupied.setter
     def update_status(self, status: int):
-        if status not in (0, 1):
-            raise ValueError(f"Room Status can only be 0 for free or 1 for occupied, got {status}")
-        else:
-            self.__occupied = status
-        con = connection
-        curs = cursor
-
-        query = "UPDATE rooms SET occupied = %d WHERE id = %d"
-        params = (self.occupied(), self.id())
-
-        curs.execute(query, params)
-        con.commit()
-
-        curs.close()
-        con.close()
+        try:
+            if status not in (0, 1):
+                raise ValueError(f"Room Status can only be 0 for free or 1 for occupied, got {status}")
+            else:
+                self.__occupied = status
+            con = connection
+            curs = cursor
+    
+            query = "UPDATE rooms SET occupied = %d WHERE id = %d"
+            params = (self.occupied(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
     
     @property
     def bed_qty(self):
         return self.__bed_qty
 
+    @bed_qty.setter
+    def bed_qty(self, qnty):
+        try:
+            if !(qnty.isdigit()) or qnty <= 0:
+                raise ValueError(f"Bed quantity must be a number and greater than 0")
+            else:
+                self.__bed_qty = qnty
+            con = connection
+            curs = cursor
+    
+            query = "UPDATE rooms SET bed_qty = %d WHERE id = %d"
+            params = (self.bed_qty(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
+
     def add_room(self):
-        con = connection
-        curs = cursor
-
-        query = "INSERT INTO rooms(bed_qty, category, type, description, price_per_night) VALUES (%d, %s, %s, %s, %f)"
-        params = (self.bed_qty, self.category, self.type, self.description, self.price_per_night)
-        
-        curs.execute(query, params)
-        con.commit()
-
-        curs.close()
-        con.close()
+        try:
+            con = connection
+            curs = cursor
+    
+            query = "INSERT INTO rooms(bed_qty, category, type, description, price_per_night) VALUES (%d, %s, %s, %s, %f)"
+            params = (self.bed_qty, self.category, self.type, self.description, self.price_per_night)
+            
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except Exception as e:
+            print(f"Error: {e}")
 
 
 class People:
@@ -67,6 +145,82 @@ class People:
     def id(self):
         return self.__id
 
+    @property
+    def f_name(self):
+        return self._f_name
+
+    @f_name.setter
+    def f_name(self, name, table: str):
+        try:
+            if len(name) > 16:
+                raise ValueError(f"The first name must have a maximum of 16 characters")
+            else:
+                self._f_name = name
+            con = connection
+            curs = cursor
+    
+            query = f"UPDATE {table} SET f_name = %s WHERE id = %d"
+            params = (self._f_name(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
+
+    @property
+    def l_name(self):
+        return self._f_name
+
+    @l_name.setter
+    def l_name(self, name, table: str):
+        try:
+            if len(name) > 16:
+                raise ValueError(f"The last name must have a maximum of 16 characters")
+            else:
+                self._l_name = name
+            con = connection
+            curs = cursor
+    
+            query = f"UPDATE {table} SET l_name = %s WHERE id = %d"
+            params = (self._l_name(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
+
+    @property
+    def email(self):
+        return self._f_name
+
+    @email.setter
+    def email(self, value, table: str):
+        try:
+            pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+            if re.fullmatch(pattern, value) == false:
+                raise ValueError(f"The Email must have a correct format")
+            else:
+                self._email = email
+            con = connection
+            curs = cursor
+    
+            query = f"UPDATE {table} SET email = %s WHERE id = %d"
+            params = (self._email(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
+
 
 class Clients(People):
     def __init__(self, id, f_name, l_name, email, nationality, birth_date):
@@ -76,17 +230,20 @@ class Clients(People):
         self.add_client()
 
     def add_client(self):
-        con = connection
-        curs = cursor
-
-        query = "INSERT INTO clients(f_name, l_name, email, birth_date, nationality) VALUES (%s, %s, %s, %s, %s)"
-        params = (self._f_name, self._l_name, self._email, self.birth_date, self.nationality)
-        
-        curs.execute(query, params)
-        con.commit()
-
-        curs.close()
-        con.close()
+        try:
+            con = connection
+            curs = cursor
+    
+            query = "INSERT INTO clients(f_name, l_name, email, birth_date, nationality) VALUES (%s, %s, %s, %s, %s)"
+            params = (self._f_name, self._l_name, self._email, self.birth_date, self.nationality)
+            
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except Exception as e:
+            print(f"Error: {e}")
 
 
 class Users(People):
@@ -96,51 +253,107 @@ class Users(People):
         self.role = role
 
     def add_user(self):
-        con = connection
-        curs = cursor
-
-        query = "INSERT INTO users(f_name, l_name, email, password, role) VALUES (%s, %s, %s, %s, %s)"
-        params = (self._f_name, self._l_name, self._email, self.__password, self.role)
-        
-        curs.execute(query, params)
-        con.commit()
-
-        curs.close()
-        con.close()
+        try:
+            con = connection
+            curs = cursor
+    
+            query = "INSERT INTO users(f_name, l_name, email, password, role) VALUES (%s, %s, %s, %s, %s)"
+            params = (self._f_name, self._l_name, self._email, self.__password, self.role)
+            
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except Exception as e:
+            print(f"{e}")
 
 
 class Reservations:
-    def __init__(self, id, start_date: datetime, end_date: datetime, status: str, total_price: float):
+    def __init__(self, id, client_id, start_date: datetime, end_date: datetime, status: str, total_price: float):
         self.__id = id
         self.start_date = start_date
         self.end_date = end_date
-        self.status = status
+        self.__status = status
         self.total_price = total_price
+        self.__client_id = client_id
+        self.__room_id = room_id
 
     @property
     def id(self):
         return self.__id
+
+    @property
+    def status(self):
+        return self.__status
     
-    def add_reservation(client: Clients, room: Rooms, start_date: datetime, end_date: datetime):
-        con = connection
-        curs = cursor
-
-        query = "INSERT INTO reservations(client_id, room_id, start_date, end_date) VALUES (%d, %d, %s, %s)"
-        params = (client.id(), room.id(), start_date.strftime('%Y-%m-%d %H:%M:%S'), end_date.strftime('%Y-%m-%d %H:%M:%S'))
-        curs.execute(query, params)
-        con.commit()
-
-        curs.close()
-        con.close()
+    @status.setter
+    def status(self, status: str):
+        try:
+            if status not in ["Checked In", "Checked Out", "Transfered", "Canceled"]:
+                raise ValueError(f"Reservation Status can only be 'Checked In', 'Checked Out', 'Transfered' or 'Canceled', got {status}")
+            else:
+                self.__status = status
+            con = connection
+            curs = cursor
+    
+            query = "UPDATE reservations SET status = %s WHERE id = %d"
+            params = (self.status(), self.id())
+    
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except ValueError as e:
+            print(e)
+            
+    def add_reservation(self, start_date: datetime, end_date: datetime):
+        try:
+            con = connection
+            curs = cursor
+    
+            query = "INSERT INTO reservations(client_id, room_id, start_date, end_date) VALUES (%d, %d, %s, %s)"
+            params = (self.__client_id, room.id(), start_date.strftime('%Y-%m-%d %H:%M:%S'), end_date.strftime('%Y-%m-%d %H:%M:%S'))
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except Exception as e:
+            print(f"{e}")
 
     def update_reservation(self, status: str):
-        con = connection
-        curs = cursor
+        try:
+            con = connection
+            curs = cursor
+    
+            query = "UPDATE reservations SET status = %s WHERE id = %d AND status = %s"
+            params = (status, self.id(), self.status)
+            curs.execute(query, params)
+            con.commit()
+    
+            curs.close()
+            con.close()
+        except Exception as e:
+            print(f"{e}")
 
-        query = "UPDATE reservations SET status = %s WHERE id = %d AND status = %s"
-        params = (status, self.id(), self.status)
-        curs.execute(query, params)
-        con.commit()
+    def get_client_name(self):
+        try:
+            con = connection
+            curs = cursor
+    
+            query = "SELECT CONCAT(f_name, ' ', l_name) AS full_name FROM clients WHERE clients.id = %d"
+            params = (self.__client_id)
+            curs.execute(query, params)
+            con.commit()
+            
+            result = curs.fetchall()
+            user_full_name = result[0][0]
+            
+            curs.close()
+            con.close()
 
-        curs.close()
-        con.close()
+            return user_full_name
+        except Exception as e:
+            print(f"{e}")
